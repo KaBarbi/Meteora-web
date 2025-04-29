a2f06ae203827bd2d4860b0d9989f38e86e32f0b
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-04-29 17:06:25 -0300

    [FIX]: bugs carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CarrinhoProvider } from "./context/CarrinhoContext";
-import Home from "./pages/Home";
-import Carrinho from "./pages/Carrinho";
-import PaginaErro from "./pages/PaginaErro";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { CarrinhoContext, CarrinhoProvider } from "./context/CarrinhoContext"
+import Home from "./pages/Home"
+import PaginaErro from "./pages/PaginaErro"
 
-import "./App.css";
+import "./App.css"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <CarrinhoProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/carrinho" element={<Carrinho />} />
-          <Route path="*" element={<PaginaErro />} />
-        </Routes>
-      </CarrinhoProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <CarrinhoProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/carrinho" element={<CarrinhoContext />} />
+                    <Route path="*" element={<PaginaErro />} />
+                </Routes>
+            </CarrinhoProvider>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
